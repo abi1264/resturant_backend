c5fbabcf6d5b7b97f79e9aca802d8402dcec0935
abi <thapaabi180@gmail.com>
2026-08-18 16:25:03 +0545

    format corrected

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 @Module({
-   imports:[] ,
-   controllers:[],
-   providers:[PrismaService]
+  imports: [],
+  controllers: [],
+  providers: [PrismaService],
 })
-export class PrismaModule{}
+export class PrismaModule {}
